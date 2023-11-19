6662a1c6ff0b3a901b7a6589611b5d12cd020ca2
Aliany Crespo <alianycres@gmail.com>
2023-11-19 14:19:55 +0100

    create the superhero service and interfaces

---
+++ src/app/superheros/interfaces/superheros.interface.ts
@@ -0,0 +1,8 @@
+export interface Superhero {
+  id?:              string;
+  superhero:        string;
+  alter_ego:        string;
+  first_appearance: string;
+  characters:       string;
+  alt_img?:         string; // alamcena el path de la imagen
+}
---
+++ src/app/superheros/services/superheros.service.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from 'src/enviroments/enviroments';
+import { Superhero } from '../interfaces/superheros.interface';
+
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SuperherosService {
+  private baseUrl: string = environment.baseUrl;
+
+  constructor( private http: HttpClient) { }
+
+  getHeroes(): Observable<Superhero[]>{
+    return this.http.get<Superhero[]>( `${ this.baseUrl }/superheros`);
+  }
+
+  getHeroePorId( id: string): Observable<Superhero>{
+    return this.http.get<Superhero>(`${ this.baseUrl }/superhero/${id}`);
+  }
+
+  getSugerencias(termino: string): Observable<Superhero[]>{
+    return this.http.get<Superhero[]>(`${ this.baseUrl }/superheros/?q=${termino}&_limit=6`);
+  }
+
+  agregarHeroe(superhero: Superhero): Observable<Superhero>{
+    return this.http.post<Superhero>(`${ this.baseUrl }/superhero`, superhero)
+  }
+
+  actualizarHeroe(superhero: Superhero): Observable<Superhero>{
+    return this.http.put<Superhero>(`${ this.baseUrl }/superhero/${superhero.id}`, superhero)
+  }
+
+  eliminarHeroe(id: string): Observable<any>{
+    return this.http.delete<any>(`${ this.baseUrl }/superhero/${id}`);
+  }
+}
